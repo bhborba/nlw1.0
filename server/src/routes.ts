fa16e67a0214de11fe6440f3dc1b7f9f93db6f28
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2020-06-06 17:05:09 -0300

    02: Olhando as oportunidades

---
+++ server/src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const itemsController = new ItemsController();
+const pointsController = new PointsController();
+
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log('Listagem de usuários'),
+app.use(cors());
+app.use(express.json());
 
-    res.json({message: 'Hello World'});
-});
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
